7582ef50909fcb1d0f5c142a8daa2eba33260837
Tsukiyo <penny911030@gmail.com>
2023-09-21 00:51:56 +0800

    save part zzz...

--- summer.php
+++ summer.php
@@ -1,19 +1,19 @@
 <?php
-$RandNum[0]=1;
-$RandNum[1]=4;
-$RandNum[2]=9;
-//print "No.1".$RandNum[0].",".$RandNum[1].",".$RandNum[2]."</p>";
-$Ans1=$RandNum[1]-$RandNum[0];
-$Ans1=abs($Ans1);
-$Ans2=$RandNum[2]-$RandNum[1];
-$Ans2=abs($Ans2);
-echo $Ans1.",".$Ans2;
+// $RandNum[0]=1;
+// $RandNum[1]=4;
+// $RandNum[2]=9;
+// $Ans1=$RandNum[1]-$RandNum[0];
+// $Ans1=abs($Ans1);
+// $Ans2=$RandNum[2]-$RandNum[1];
+// $Ans2=abs($Ans2);
+// echo $Ans1.",".$Ans2;
 $Fre=0;
 do{
     for($j=0;$j<=10;$j++){
         $Fre=$Fre+1;
         for($i=0;$i<3;$i++){
             $RandNum[$i] = rand(0,9);
+            
         };
     $Ans1=$RandNum[1]-$RandNum[0];
     $Ans1=abs($Ans1);
@@ -27,5 +27,4 @@ do{
     };
 }while($Ans1 !== $Ans2 && $Fre<=9);
 print  "超過10次或已經找到數字了喔~";
-
 ?>
