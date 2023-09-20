a4eb0f4d99dfa2794dd37a294be5086d3287424e
Tsukiyo <penny911030@gmail.com>
2023-09-20 23:51:52 +0800

    use do...while

--- summer.php
+++ summer.php
@@ -9,23 +9,22 @@ $Ans2=$RandNum[2]-$RandNum[1];
 $Ans2=abs($Ans2);
 echo $Ans1.",".$Ans2;
 $Fre=0;
-if($Ans1 !== $Ans2){
+do{
     for($j=0;$j<10;$j++){
         $Fre=$Fre+1;
         for($i=0;$i<3;$i++){
             $RandNum[$i] = rand(0,9);
         };
-$Ans1=$RandNum[1]-$RandNum[0];
-$Ans1=abs($Ans1);
-$Ans2=$RandNum[2]-$RandNum[1];
-$Ans2=abs($Ans2);
+    $Ans1=$RandNum[1]-$RandNum[0];
+    $Ans1=abs($Ans1);
+    $Ans2=$RandNum[2]-$RandNum[1];
+    $Ans2=abs($Ans2);
     print_r($RandNum);
     print "</p>";
     print $Fre." = ".$RandNum[0]." , ".$RandNum[1]." , ".$RandNum[2]."</p>";
     echo $Ans1.",".$Ans2."</p>";
-
+        break;
     };
- }else{
-  print  "超過10次或已經找到數字了喔~";
- };
+}while($Ans1 !== $Ans2);
+print  "超過10次或已經找到數字了喔~";
 ?>
