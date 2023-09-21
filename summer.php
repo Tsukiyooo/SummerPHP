48f360254d7850f87a5efc912ce84d1734636970
Tsukiyo <penny911030@gmail.com>
2023-09-21 18:04:01 +0800

    R Num not re

--- summer.php
+++ summer.php
@@ -1,22 +1,35 @@
-<?php
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP 頁面上的按鈕</title>
+</head>
+<body>
+<form method="post" action="">
+        <button type="submit" name="submit">送出</button>
+    </form>
+    </br>
+    <?php
+     if (isset($_POST['submit'])){ 
 session_start();
 $Fre=0;
-$RandNum=array();
 do{
     for($j=0;$j<=10;$j++){
+        $RandNum=array();
         $Fre=$Fre+1;
-        for($i=0;$i<3;$i++){
+        do{
             $Run=rand(0,9);
             if(!in_array($Run,$RandNum)){
-                $RandNum[$i]=$Run;
-            }
-                print $RandNum[$i];            
-        };
+                $RandNum[]=$Run;
+            };
+
+        }while(count($RandNum)<3);
+        print_r ($RandNum);            
     $Ans1=$RandNum[1]-$RandNum[0];
     $Ans1=abs($Ans1);
     $Ans2=$RandNum[2]-$RandNum[1];
     $Ans2=abs($Ans2);
-    print_r($RandNum);
+    //print_r($RandNum);
     print "</p>";
     print $Fre." = ".$RandNum[0]." , ".$RandNum[1]." , ".$RandNum[2]."</p>";
     echo $Ans1.",".$Ans2."</p>";
@@ -26,5 +39,12 @@ do{
     };
 }while($Ans1 !== $Ans2 && $Fre<=9);
 print  "超過10次或已經找到數字了喔~";
+};
 
+
+//include 'page.php';
 ?>
+
+
+</body>
+</html>
