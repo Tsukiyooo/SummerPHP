84eac385174826306799c1f9d9e2b526afa14f42
Tsukiyo <penny911030@gmail.com>
2023-09-21 16:50:59 +0800

    Num not re

--- summer.php
+++ summer.php
@@ -1,19 +1,16 @@
 <?php
-// $RandNum[0]=1;
-// $RandNum[1]=4;
-// $RandNum[2]=9;
-// $Ans1=$RandNum[1]-$RandNum[0];
-// $Ans1=abs($Ans1);
-// $Ans2=$RandNum[2]-$RandNum[1];
-// $Ans2=abs($Ans2);
-// echo $Ans1.",".$Ans2;
+session_start();
 $Fre=0;
+$RandNum=array();
 do{
     for($j=0;$j<=10;$j++){
         $Fre=$Fre+1;
         for($i=0;$i<3;$i++){
-            $RandNum[$i] = rand(0,9);
-            
+            $Run=rand(0,9);
+            if(!in_array($Run,$RandNum)){
+                $RandNum[$i]=$Run;
+            }
+                print $RandNum[$i];            
         };
     $Ans1=$RandNum[1]-$RandNum[0];
     $Ans1=abs($Ans1);
@@ -23,8 +20,11 @@ do{
     print "</p>";
     print $Fre." = ".$RandNum[0]." , ".$RandNum[1]." , ".$RandNum[2]."</p>";
     echo $Ans1.",".$Ans2."</p>";
+    $_SESSION["Fre"]=$Fre;//session項次,紀錄總共幾次了
+    //print "</p>".$_SESSION["Fre"]."</p>";
         break;
     };
 }while($Ans1 !== $Ans2 && $Fre<=9);
 print  "超過10次或已經找到數字了喔~";
+
 ?>
